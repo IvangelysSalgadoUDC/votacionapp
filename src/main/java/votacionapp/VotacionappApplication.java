dcc81576d39217ca1be10aae80774a2fcf615a7a
Ivangelys Salgado <isalgadog@unicartagena.edu.co>
2026-05-28 21:29:34 -0500

    Configuracion inicial proyecto Spring Boot MVC

---
+++ src/main/java/votacionapp/VotacionappApplication.java
@@ -0,0 +1,13 @@
+package votacionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionappApplication.class, args);
+	}
+
+}
