543db8897d42c7b681f567da1468aca327e8719d
Ivangelys Salgado <isalgadog@unicartagena.edu.co>
2026-05-30 08:37:26 -0500

    feat: intercambio de datos Model y @Value - guia 5

--- src/main/java/votacionapp/ControladorInicio.java
+++ src/main/java/votacionapp/ControladorInicio.java
@@ -2,7 +2,9 @@ package votacionapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +12,14 @@ public class ControladorInicio {
 
     private static final Logger log = LoggerFactory.getLogger(ControladorInicio.class);
 
+    @Value("${index.mensaje}")
+    String dato;
+
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model modelo) {
+        String mensaje = "Saludos desde Spring MVC con paso de información";
+        modelo.addAttribute("mensaje", mensaje);
+        modelo.addAttribute("dato", dato);
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
     }
