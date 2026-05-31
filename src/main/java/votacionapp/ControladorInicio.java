fe2a8f9d5ef0f253c5dce1e907dd2b0c6fa82067
Ivangelys Salgado <isalgadog@unicartagena.edu.co>
2026-05-31 17:47:18 -0500

    feat: modelo Usuario e intercambio de objetos - guia 6

--- src/main/java/votacionapp/ControladorInicio.java
+++ src/main/java/votacionapp/ControladorInicio.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import votacionapp.modelo.Usuario;
 
 @Controller
 public class ControladorInicio {
@@ -20,6 +21,14 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC con paso de información";
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
+
+        Usuario u = new Usuario();
+        u.setCedula("1234677");
+        u.setClave("Abcd");
+        u.setNombre("Ivangelys Victoria Salgado Gutierrez");
+        u.setEmail("ivangelissalgado@gmail.com");
+        modelo.addAttribute("alguien", u);
+
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
     }
