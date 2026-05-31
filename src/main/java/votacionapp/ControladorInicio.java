355cbb5a1e5162ec03152ed31e115cdfdd185239
Ivangelys Salgado <isalgadog@unicartagena.edu.co>
2026-05-31 18:12:57 -0500

    feat: lista de objetos Usuario en tabla Thymeleaf - guia 7

--- src/main/java/votacionapp/ControladorInicio.java
+++ src/main/java/votacionapp/ControladorInicio.java
@@ -1,11 +1,14 @@
 package votacionapp;
 
+import java.util.Arrays;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import votacionapp.modelo.Usuario;
 
 @Controller
 public class ControladorInicio {
@@ -20,6 +23,29 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC con paso de información";
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
+
+        Usuario u1 = new Usuario();
+        u1.setCedula("1234");
+        u1.setClave("Abcd");
+        u1.setNombre("Ivangelys Victoria Salgado Gutierrez");
+        u1.setEmail("ivangelissalgado@gmail.com");
+        modelo.addAttribute("alguien", u1);
+
+        Usuario u2 = new Usuario();
+        u2.setCedula("777");
+        u2.setClave("El mejor");
+        u2.setNombre("JESUS DE NAZARET");
+        u2.setEmail("jesuscristo@iglesia.com");
+
+        Usuario u3 = new Usuario();
+        u3.setCedula("4321");
+        u3.setClave("*xyz*");
+        u3.setNombre("FULANITO DE TAL");
+        u3.setEmail("fulanito@gmail.com");
+
+        List<Usuario> listaUsuarios = Arrays.asList(u2, u3);
+        modelo.addAttribute("usuarios", listaUsuarios);
+
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
     }
---
+++ src/main/java/votacionapp/modelo/Usuario.java
@@ -0,0 +1,34 @@
+package votacionapp.modelo;
+
+public class Usuario {
+
+    private String cedula;
+    private String clave;
+    private String nombre;
+    private String email;
+
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public String getClave() {
+        return clave;
+    }
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
